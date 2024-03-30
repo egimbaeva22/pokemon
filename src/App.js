c4fcaff6eeeded51f535edfdcc9eada0b9da9193
egimbaeva22 <alinaegimbaeva2006@gmail.com>
2024-03-30 17:00:29 +0600

    pokemonurl

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('https://pokeapi.co/api/v2/pokemon/')
+                const responseData = response.data.results
+                console.log(responseData)
+                const imgData = await Promise.all(responseData.map(async(pokemon) => {
+                    const urlData = await axios.get(pokemon.url)
+                    return urlData.data
+                }))
+                setData(imgData)
+            }catch (e) {
+                console.log('ошибка', e)
+            }
+
+        }
+        fetchData()
+    }, []);
+    return (
+        <div style={{ display: 'flex', gap: '10px', flexWrap: 'wrap' }}>
+            {data && data.map((item, index) => (
+                <div key={index} style={{ border: '3px solid black', borderRadius: '10px', padding: '10px', width: '200px' }}>
+                    <h1 style={{ textAlign: 'center', marginBottom: '10px' }}>{item.name}</h1>
+                    {item.sprites && (
+                        <div style={{ display: 'flex', justifyContent: 'center' }}>
+                            <img src={item.sprites.front_default} alt={item.name} style={{ maxWidth: '100%', height: 'auto' }} />
+                        </div>
+                    )}
+                </div>
+            ))}
+        </div>
+    );
+};
 
 export default App;
